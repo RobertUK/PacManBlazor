c01979c9f1e595cf6d7e797ed38d6693b1455fb9
itthings <44327454+itthings@users.noreply.github.com>
2023-01-05 19:29:21 +0000

    .

--- src/PacMan/wwwroot/service-worker.js
+++ src/PacMan/wwwroot/service-worker.js
@@ -1,35 +1,11 @@
-// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-//self.addEventListener('fetch', () => { });
 
-//self.assetsInclude = [];
-//self.assetsExclude = [/\.scp\.css$/, /weather\.json$/];
-//self.defaultUrl = 'index.html';
-//self.prohibitedUrls = [];
-//self.assetsUrl = '/service-worker-assets.js';
+self.assetsInclude = [/\.wav$/];
 
-//// more about SRI (Subresource Integrity) here: https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity
-//// online tool to generate integrity hash: https://www.srihash.org/   or   https://laysent.github.io/sri-hash-generator/
-//// using only js to generate hash: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
-//self.externalAssets = [
-//    //{
-//    //    "hash": "sha256-lDAEEaul32OkTANWkZgjgs4sFCsMdLsR5NJxrjVcXdo=",
-//    //    "url": "css/app.css"
-//    //},
-//    {
-//        "url": "/"
-//    },
-//];
 
-//self.caseInsensitiveUrl = true;
+//const DEFAULT_ASSETS_INCLUDE = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/, /\.svg$/, /\.woff2$/, /\.ttf$/];
+//const DEFAULT_ASSETS_EXCLUDE = [/^_content\/Bit.Bswup\/bit-bswup.sw.js$/, /^service-worker\.js$/];
 
-//self.serverHandledUrls = [/\/api\//];
-//self.serverRenderedUrls = [/\/privacy$/];
 
-//self.importScripts('_content/Bit.Bswup/bit-bswup.sw.js');
-
-self.assetsInclude = [];
 self.assetsExclude = [/\.scp\.css$/, /weather\.json$/];
 self.defaultUrl = 'index.html';
 self.prohibitedUrls = [];
@@ -47,7 +23,7 @@ self.externalAssets = [
         "url": "/"
     },
 ];
-self.sk
+
 self.caseInsensitiveUrl = true;
 
 self.serverHandledUrls = [/\/api\//];
